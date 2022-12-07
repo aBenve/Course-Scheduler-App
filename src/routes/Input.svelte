e71c2e35c76e4cb6f1d6ae215641647e60cba5b7
Alan Sartorio <alan42ga@hotmail.com>
2022-12-07 01:37:42 -0300

    Implemented simple plan selection:
    Moved graph component to separate file.
    Added plan selector dropdowns.
    Fixed loading in Input page.

--- src/routes/Input.svelte
+++ src/routes/Input.svelte
@@ -4,6 +4,8 @@
     SubjectPlan,
   } from "@course-scheduler-app/scheduler-wasm";
   import type { Simulation, SimulationNodeDatum } from "d3";
+  import PlanSelector from "src/components/PlanSelector.svelte";
+  import SubjectGraph from "src/components/SubjectGraph.svelte";
   import selected, { toggleSubject } from "src/store/GraphSelectedStore";
   import LinkButton from "../components/LinkButton.svelte";
   import LoadingSpinner from "../components/LoadingSpinner.svelte";
@@ -13,82 +15,6 @@
   import colors from "../utils/colors";
   import { graph } from "../utils/graph";
 
-  let svgElem: Element;
-  let simulation: Simulation<SimulationNodeDatum, undefined> | undefined;
-
-  let charge = 0,
-    link = 0;
-
-  let width: number, height: number;
-  $: simulation !== undefined && resizedSvg(width, height);
-  function resizedSvg(width: number, height: number) {
-    simulation.force("x").x(width / 2);
-    simulation.force("y").y(height / 2);
-    simulation.alpha(1);
-    simulation.restart();
-  }
-
-  $: changeChargeStrength(charge);
-  function changeChargeStrength(charge: number) {
-    if (simulation !== undefined) simulation.force("charge").strength(charge);
-  }
-
-  $: changeLinkStrength(link);
-  function changeLinkStrength(link: number) {
-    if (simulation !== undefined) simulation.force("link").strength(link);
-  }
-
-  $: courseCommissions = $courseCommissionsStore;
-
-  $: $planStore !== null && svgElem !== undefined && planChanged($planStore);
-  function planChanged(plan: SubjectPlan) {
-    // console.log("Plan changed");
-    const deselectedColor = colors[0];
-    const selectedColor = colors[1];
-    const subjects = plan.get_subjects();
-
-    let nodes = subjects.map((code, i) => {
-      const info = plan.get_subject_info(code);
-      const node = {
-        id: info.code,
-        label: info.name,
-      };
-      info.free();
-      return node;
-    });
-    let edges = subjects.flatMap((code) =>
-      plan
-        .get_subject_dependencies(code)
-        .map((dep) => ({ source: dep, target: code }))
-    );
-
-    simulation = graph(svgElem, nodes, edges, width, height, (node) => {
-      const code = node.id;
-      toggleSubject(code);
-    });
-  }
-
-  $: simulation !== undefined &&
-    $selected !== null &&
-    selectedChanged($selected);
-
-  function selectedChanged(selected: Set<string>) {
-    simulation.nodes().forEach((node) => {
-      node.selected = selected.has(node.id);
-    });
-  }
-
-  $: simulation !== undefined &&
-    $courseCommissionsStore !== null &&
-    courseCommissionsChanged($courseCommissionsStore);
-
-  function courseCommissionsChanged(courseCommissions: Commissions) {
-    simulation.nodes().forEach((node) => {
-      node.available =
-        courseCommissions.get_subject_info(node.id) !== undefined;
-    });
-  }
-
   $: loading = $courseCommissionsStore === null || $planStore === null;
 </script>
 
@@ -96,21 +22,18 @@
   class="flex justify-center items-center h-screen bg-background dark:bg-background-dark colorTransition"
 >
   {#if loading}
-    <div class="absolute inset-0 flex bg-black/50 items-center justify-center">
-      <LoadingSpinner />
-    </div>
+    <!--<div class="absolute inset-0 flex bg-black/50 items-center justify-center">-->
+    <LoadingSpinner />
+    <!--</div>-->
   {:else}
-    <div
-      class="w-full h-full"
-      bind:clientWidth={width}
-      bind:clientHeight={height}
-    >
-      <svg bind:this={svgElem} width="100%" height="100%" />
-    </div>
-    <div class="absolute right-5 bottom-5">
-      <LinkButton title="Start" link="Options" />
-    </div>
+    <SubjectGraph />
   {/if}
+  <div class="absolute left-5 top-5">
+    <PlanSelector />
+  </div>
+  <div class="absolute right-5 bottom-5">
+    <LinkButton title="Start" link="Options" />
+  </div>
   <!-- <div
     class="absolute left-2 top-2 flex flex-col items-center w-fit h-fit bg-zone rounded-lg p-5"
   >
