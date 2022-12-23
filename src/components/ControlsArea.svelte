d212062960360b6febcb64cdfe9d28513ac2dbf5
aBenve <abenvenuto@itba.edu.ar>
2022-12-22 21:34:02 -0300

    Forgot this file

--- src/components/ControlsArea.svelte
+++ src/components/ControlsArea.svelte
@@ -3,6 +3,7 @@
   import { Link } from "svelte-routing";
   import { fly } from "svelte/transition";
   import ControlContent from "./ControlContent.svelte";
+  import LinkButton from "./LinkButton.svelte";
   let clazz: string;
   export { clazz as class };
 
@@ -16,16 +17,20 @@
 <main class={clazz}>
   <div
     in:fly={{ x: -10, duration: 500, delay: 200 }}
-    class="bg-area dark:bg-area-dark h-full p-5 rounded-lg flex flex-col items-center gap-y-5 colorTransition"
+    class="bg-area dark:bg-area-dark h-full p-4 rounded-lg flex flex-col items-center gap-y-5 colorTransition"
   >
     <div class="flex w-full">
-      <Link to="/">
+      <Link
+        to="/"
+        class="flex space-x-2 items-center hover:bg-zone-secondary dark:hover:bg-zone-secondary-dark hover:bg-opacity-50 p-2 rounded-lg py-2 px-3"
+      >
         <Icon
           icon="material-symbols:arrow-back-ios-new-rounded"
-          width="18"
-          height="18"
-          class="text-text-terciary"
+          width="12"
+          height="12"
+          class="text-text-dark dark:text-text"
         />
+        <span class="text-text-dark dark:text-text text-xs">Go back</span>
       </Link>
     </div>
     <ControlContent />
