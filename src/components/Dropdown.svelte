96169b1117e8c704a2e1f8a377ecd6a58501b802
aBenve <abenvenuto@itba.edu.ar>
2023-01-16 14:30:58 -0300

    Better dropdown using focus

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import Icon from "@iconify/svelte";
   import { createEventDispatcher } from "svelte";
 
@@ -46,4 +46,42 @@
       {/each}
     </div>
   {/if}
+</div> -->
+<script>
+  import Icon from "@iconify/svelte";
+  import { createEventDispatcher } from "svelte";
+
+  export let options;
+  export let selectedOption;
+
+  const onChange = createEventDispatcher();
+</script>
+
+<div
+  class="group relative flex items-center bg-zone dark:bg-zone-dark w-fit colorTransition min-w-[5rem] focus-within:rounded-b-none focus-within:rounded-t-lg rounded-lg"
+>
+  <button
+    class="group flex items-center justify-between space-x-2 px-3 py-2 text-text-dark dark:text-text text-sm  block w-full colorTransition"
+    ><span>{selectedOption}</span>
+    <Icon
+      icon="material-symbols:keyboard-arrow-down-rounded"
+      width="18"
+      height="18"
+      class="text-text-terciary group-focus-within::transform group-focus-within::rotate-180 group-focus-within::transition group-focus-within::ease-int-out group-focus-within::duration-300 transform rotate-0 transition ease-int-out duration-300"
+    /></button
+  >
+  <div
+    class="invisible group-focus-within:visible flex pt-4 flex-col space-y-2 absolute w-full bg-area dark:bg-area-dark colorTransition  rounded-b-lg rounded-t-none  top-full left-0 "
+  >
+    {#each options as option}
+      <button
+        on:click={() => {
+          selectedOption = option;
+          onChange("change", option);
+        }}
+        class="hover:bg-zone hover:dark:bg-zone-dark text-text-terciary hover:text-accent last:hover:rounded-b-lg text-sm py-1 px-2 block w-full  colorTransition"
+        >{option}</button
+      >
+    {/each}
+  </div>
 </div>
