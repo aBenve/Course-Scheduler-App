3269939539034e8c0a3888068dfdd5c9dd6485b8
aBenve <abenvenuto@itba.edu.ar>
2023-01-16 14:33:38 -0300

    Better popup for sliders.

--- src/components/ConfigurationSliderButton.svelte
+++ src/components/ConfigurationSliderButton.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
     import CustomOptionInput from "./CustomOptionInput.svelte";
-
-    export let onClick: () => void;
-    export let isOpen = false;
     export let min_label: string;
     export let max_label: string;
     export let placeholder: string;
@@ -13,26 +10,23 @@
 </script>
 
 <div
-    class="relative  md:static md:block flex flex-col gap-4 h-[3rem] w-full md:w-auto"
+    class="relative group  md:static md:block flex flex-col gap-4 h-[3rem] w-full md:w-auto"
 >
     <button
-        on:click={() => onClick()}
-        class="rounded-lg text-sm h-full py-3 px-4 bg-area dark:bg-area-dark hover:bg-zone dark:hover:bg-zone-dark text-text-dark-secondary dark:text-text-secondary  colorTransition"
+        class="rounded-lg group text-sm h-full py-3 px-4 bg-area dark:bg-area-dark hover:bg-zone dark:hover:bg-zone-dark text-text-dark-secondary dark:text-text-secondary  colorTransition"
     >
         {buttonText}
     </button>
-    {#if isOpen}
-        <div
-            class="absolute shadow-xl md:w-[15rem] w-full z-30 mt-14 md:mt-2 bg-zone dark:bg-zone-dark p-4 rounded-lg colorTransition"
-        >
-            <CustomOptionInput
-                {min_label}
-                {max_label}
-                {placeholder}
-                {max}
-                {min}
-                text={sliderText}
-            />
-        </div>
-    {/if}
+    <div
+        class="invisible group-focus-within:visible absolute shadow-xl md:w-[15rem] w-full z-30 mt-14 md:mt-2 bg-zone dark:bg-zone-dark p-4 rounded-lg colorTransition"
+    >
+        <CustomOptionInput
+            {min_label}
+            {max_label}
+            {placeholder}
+            {max}
+            {min}
+            text={sliderText}
+        />
+    </div>
 </div>
--- src/components/ConfigurationSliderButtonList.svelte
+++ src/components/ConfigurationSliderButtonList.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
     import ConfigurationSliderButton from "./ConfigurationSliderButton.svelte";
-
-    let isOpen = null;
 </script>
 
 <div
     class="flex items-center justify-around md:justify-start gap-2 md:overflow-x-auto overflow-visible"
 >
     <ConfigurationSliderButton
-        isOpen={isOpen === 0}
-        onClick={() => {
-            isOpen = isOpen === 0 ? null : 0;
-        }}
         min_label="min_credits"
         max_label="max_credits"
         placeholder="18"
@@ -21,10 +15,6 @@
         buttonText="Credits"
     />
     <ConfigurationSliderButton
-        isOpen={isOpen === 1}
-        onClick={() => {
-            isOpen = isOpen === 1 ? null : 1;
-        }}
         min_label="gap"
         max_label="gap"
         placeholder="2"
@@ -34,10 +24,6 @@
         buttonText="Gap"
     />
     <ConfigurationSliderButton
-        isOpen={isOpen === 2}
-        onClick={() => {
-            isOpen = isOpen === 2 ? null : 2;
-        }}
         min_label="min_subjects"
         max_label="max_subjects"
         placeholder="5"
