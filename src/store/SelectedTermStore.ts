c55b7e7526b3f75c3ab29c5ed79c2f23ea85e255
Alan Sartorio <alan42ga@hotmail.com>
2023-12-12 10:08:11 -0300

    Update to First semester 2024

--- src/store/SelectedTermStore.ts
+++ src/store/SelectedTermStore.ts
@@ -2,8 +2,8 @@ import { Semester } from "@course-scheduler-app/scheduler-wasm";
 import { writable, type Writable } from "svelte/store";
 
 const selectedTerm: Writable<{ year: number; semester: Semester }> = writable({
-  year: 2023,
-  semester: Semester.Second,
+  year: 2024,
+  semester: Semester.First,
 });
 
 export default selectedTerm;
